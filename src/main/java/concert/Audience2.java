893e8192ddd698abaf653e214bf712e4c33cd8ef
Jingguo Yao <yaojingguo@gmail.com>
2015-05-05 17:42:06 +0800

    Update

---
+++ src/main/java/concert/Audience2.java
@@ -0,0 +1,26 @@
+package concert;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class Audience2 {
+  
+  @Pointcut("execution(** concert.Performance.perform(..))")
+  public void performance() {}
+
+  @Around("performance()")
+  public void watchPerformance(ProceedingJoinPoint jp) {
+    try {
+      System.out.println("Silencing cell phones");
+      System.out.println("Taking seats");
+      jp.proceed();
+      System.out.println("CLAP CLAP CLAP!!!");
+    } catch (Throwable e) {
+      System.out.println("Demanding a refund");
+    }
+  }
+  
+}
