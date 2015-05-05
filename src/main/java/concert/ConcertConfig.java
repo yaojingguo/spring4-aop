0be38299512d73514e0fd9d705c1048e0c6554c2
Jingguo Yao <yaojingguo@gmail.com>
2015-05-05 17:59:28 +0800

    Update

--- src/main/java/concert/ConcertConfig.java
+++ src/main/java/concert/ConcertConfig.java
@@ -1,13 +1,11 @@
 package concert;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @EnableAspectJAutoProxy
-@ComponentScan
 public class ConcertConfig {
   @Bean
   public Audience audience() {
