1b26880cffbe7f6cc9bd4e614be6bde4c4d17bbb
Jingguo Yao <yaojingguo@gmail.com>
2015-05-05 17:57:40 +0800

    Update

---
+++ src/main/java/nba/Fans.java
@@ -0,0 +1,24 @@
+package nba;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class Fans {
+  @Pointcut("execution(** nba.Game.play(..))")
+  public void play() {}
+
+  @Around("play()")
+  public void watchPerformance(ProceedingJoinPoint jp) {
+    try {
+      System.out.println("Silencing cell phones");
+      System.out.println("Taking seats");
+      jp.proceed();
+      System.out.println("CLAP CLAP CLAP!!!");
+    } catch (Throwable e) {
+      System.out.println("Demanding a refund");
+    }
+  }
+}
---
+++ src/main/java/nba/GameImpl.java
@@ -0,0 +1,11 @@
+package nba;
+
+import org.springframework.context.annotation.Bean;
+
+
+public class GameImpl implements Game {
+
+  @Override
+  public void play() {}
+
+}
